6ba97b5d4daabbb8f6eb43e06065b927bafa9c1e
Nait-cherif Sabrinel <162204241+sabrineeel@users.noreply.github.com>
2024-03-31 23:53:17 +0100

    Update BruteForcce_Sha1.py

--- BruteForcce_Sha1.py
+++ BruteForcce_Sha1.py
@@ -1,12 +1,45 @@
+print (""" 
+
+██████  ██████  ██    ██ ████████ ███████     ███████  ██████  ██████   ██████ ███████ 
+██   ██ ██   ██ ██    ██    ██    ██          ██      ██    ██ ██   ██ ██      ██      
+██████  ██████  ██    ██    ██    █████       █████   ██    ██ ██████  ██      █████   
+██   ██ ██   ██ ██    ██    ██    ██          ██      ██    ██ ██   ██ ██      ██      
+██████  ██   ██  ██████     ██    ███████     ██       ██████  ██   ██  ██████ ███████                                                            
+                 
+""")
 import hashlib
 import itertools
-
-
+import re
 
 def calculate_sha1(message):
     hasher = hashlib.sha1()
     hasher.update(message.encode('utf-8'))
     return hasher.hexdigest()
+
+def validate_sha1_hash(md5_hash):
+    # Vérifie que la longueur du hachage MD5 correspond à 32 caractères hexadécimaux
+    return re.match(r"^[a-fA-F0-9]{40}$", md5_hash) is not None
+
+def validate_password_length(password_length):
+    # Vérifie que la taille du mot de passe est un entier positif
+    return password_length.isdigit() and int(password_length) > 0
+
+
+while True:
+    h1 = input("Entrez le résultat de hachage Sha1 : ")
+    if validate_sha1_hash(h1):
+        break
+    else:
+        print("Le hachage sha1 entré n'est pas valide. Assurez-vous qu'il s'agit d'un hachage Sha1 de 40 caractères hexadécimaux.")
+
+while True:
+    password_length = input("Entrez la taille du mot de passe : ")
+    if validate_password_length(password_length):
+        password_length = int(password_length)  # Convertir en entier
+        break
+    else:
+        print("La taille du mot de passe entrée n'est pas valide. Assurez-vous qu'elle est un entier positif.")
+
 
 
 
@@ -16,8 +49,7 @@ def generate_combinations(characters, length):
         combinations.extend([''.join(combination) for combination in itertools.product(characters, repeat=i)])
     return combinations
 
-h1 = input("Entrez le résultat de hachage SHA-1 : ")
-password_length = int(input("Entrez la taille du mot de passe : "))
+
 
 
 characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
