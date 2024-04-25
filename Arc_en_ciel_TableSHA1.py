cda31ffa5a3e659787a929f2035cc66eedca8fc2
Djazia_Gh <150776328+DjazzGh@users.noreply.github.com>
2024-04-25 21:23:04 +0100

    Create Arc_en_ciel_TableSHA1.py

---
+++ Arc_en_ciel_TableSHA1.py
@@ -0,0 +1,13 @@
+import itertools
+import hashlib
+import os
+
+def generate_rainbow_table(charset, min_length, max_length, filename):
+    with open(filename, 'w') as f:
+        for r in range(min_length, max_length + 1):
+            for password in itertools.product(charset, repeat=r):
+                password = ''.join(password)
+                hash_value = hashlib.sha1(password.encode()).hexdigest()
+                f.write(f"{hash_value}:{password}\n")
+
+generate_rainbow_table('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',1,4, "tablesha1.txt")
